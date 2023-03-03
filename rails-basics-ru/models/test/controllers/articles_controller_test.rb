12c0dc133db40533a625649c788fe5be078575d6
@hexlet/cli <support@hexlet.io>
2023-03-03 16:52:39 +0300

    submit rails-basics-ru/models

---
+++ rails-basics-ru/models/test/controllers/articles_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get article_url(@article)
+    assert_response :success
+  end
+end
