84b796914469438048e3ab03ba85e9a5083222a7
@hexlet/cli <support@hexlet.io>
2023-03-04 13:52:27 +0300

    submit rails-basics-ru/relations

---
+++ rails-basics-ru/relations/db/seeds.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+  Status.create(
+    name: Faker::Lorem.sentence
+  )
+end
+
+3.times do |i|
+  User.create(
+    name: Faker::Name.unique.name
+  )
+end
+
+10.times do |i|
+  Task.create(
+    name: Faker::Name.unique.name,
+    description: Faker::Lorem.paragraphs.join("\n\n"),
+    user: User.all.sample,
+    status: Status.all.sample
+  )
+end
